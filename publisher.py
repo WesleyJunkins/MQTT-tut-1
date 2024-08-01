05db29ec023883b02bda9cab0a19571a338ae873
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2024-08-01 16:14:01 -0500

    add readme to this and node, and make node look like this one

--- publisher.py
+++ publisher.py
@@ -1,23 +1,36 @@
 from paho.mqtt import client as mqtt
+import time
 
 # Go to https://www.hivemq.com/mqtt/public-mqtt-broker/ for info.
 
-clientId = "Publisher"
-broker_address = "broker.hivemq.com" # The address of the broker. If using online, use a URL. If hosting yourself, use an IP address.
-port = 1883 # Default MQTT port
-websocketPort = 8000 # The port you should use if you want to incorporate websockets
+def create_client(username, password, broker_address, clientId="client", port=8883, websocketPort=8884):
+    clientId = clientId
+    broker_address = broker_address
+    port = port
+    websocketPort = websocketPort
+    username = username
+    password = password
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.username_pw_set(username, password)
+    client.tls_set()
+    client.connect(broker_address, port)
+    return client
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, clientId)
-client.connect(broker_address, port)
+def pub(client, topic, message):
+    client.publish(topic, message)
 
-# Publish to a topic
-topic = "test/myTopic"
-message = "Hello, this is Wes!"
-client.publish(topic, message)
-print(f"Published message {message} to topic {topic}")
-client.publish(topic, message)
-print(f"Published message {message} to topic {topic}")
-client.publish(topic, message)
-print(f"Published message {message} to topic {topic}")
+# vvv   your code starts here   vvv
 
+# Create client
+client = create_client("testPublisher1", "Htil2017", "ac468314de194d56906aa94b21f74655.s1.eu.hivemq.cloud")
+
+# Publish a counter message every 0.01 seconds
+counter = 0
+while True:
+    pub(client, "htil/test/topic", counter)
+    print(f"Published message {counter}")
+    counter += 1
+    time.sleep(0.01)
+
+# Loop
 client.loop_forever()
--- subscriber.py
+++ subscriber.py
@@ -1,23 +1,37 @@
-import paho.mqtt.client as mqtt
+from paho.mqtt import client as mqtt
+import time
 
 # Go to https://www.hivemq.com/mqtt/public-mqtt-broker/ for info.
 
+def create_client(username, password, broker_address, clientId="client", port=8883, websocketPort=8884):
+    clientId = clientId
+    broker_address = broker_address
+    port = port
+    websocketPort = websocketPort
+    username = username
+    password = password
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.username_pw_set(username, password)
+    client.tls_set()
+    client.connect(broker_address, port)
+    return client
+
+def sub(client, topic):
+    client.subscribe(topic)
+    client.on_message = on_message
+
+# ~~~   your code starts here   ~~~
+
+# Message handler
 def on_message(client, userdata, message):
     print(f"Received message {message.payload.decode()} on topic {message.topic}")
 
-clientId = "Subscriber"
-broker_address = "broker.hivemq.com"
-port = 1883
-websocketPort = 8000
-
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, clientId)
-client.on_message = on_message
-client.connect(broker_address, port)
+# Create client
+client = create_client("testPublisher1", "Htil2017", "ac468314de194d56906aa94b21f74655.s1.eu.hivemq.cloud")
 
 # Subscribe to a topic
-topic = "test/myTopic"
-client.subscribe(topic)
-print(f"Subscribed to topic {topic}")
+sub(client, "htil/test/topic")
+print(f"Subscribed to topic")
 
+# Loop
 client.loop_forever()
-
